1891e9ed0b659006514121e674d6c1be26bf85c0
doodzik <frederik.dudzik@gmail.com>
2015-05-24 23:30:49 +0200

    clean up package.json

--- preprocessor.js
+++ preprocessor.js
@@ -1,17 +1,15 @@
 // preprocessor.js
-var coffee     = require('coffee-script');
-var ReactTools = require('react-tools');
-var babel = require("babel");
+var coffee = require('coffee-script')
+var babel  = require("babel")
 
 module.exports = {
   process: function(src, path) {
     // CoffeeScript files can be .coffee, .litcoffee, or .coffee.md
     if (coffee.helpers.isCoffee(path)) {
-      return coffee.compile(src, {'bare': true});
+      return coffee.compile(src, {'bare': true})
     } else {
-      return babel.transform(src).code;
-      //return ReactTools.transform(src);
+      return babel.transform(src).code
     }
-    return src;
+    return src
   }
 };
