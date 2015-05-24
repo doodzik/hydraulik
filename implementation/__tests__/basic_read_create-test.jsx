5aca869e0b8224f47ec06aa12625275abcee8af4
doodzik <frederik.dudzik@gmail.com>
2015-05-25 01:24:14 +0200

    made tests faster

--- implementation/__tests__/basic_read_create-test.jsx
+++ implementation/__tests__/basic_read_create-test.jsx
@@ -12,13 +12,11 @@ describe('CheckboxWithLabel', function() {
     );
 
     // Verify that it's Off by default
-    var label = TestUtils.findRenderedDOMComponentWithTag(
-      checkbox, 'label');
+    var label = TestUtils.findRenderedDOMComponentWithTag(checkbox, 'label');
     expect(label.getDOMNode().textContent).toEqual('Off');
 
     // Simulate a click and verify that it is now On
-    var input = TestUtils.findRenderedDOMComponentWithTag(
-      checkbox, 'input');
+    var input = TestUtils.findRenderedDOMComponentWithTag(checkbox, 'input');
     TestUtils.Simulate.change(input);
     expect(label.getDOMNode().textContent).toEqual('On');
   });
--- preprocessor.js
+++ preprocessor.js
@@ -4,12 +4,12 @@ var babel  = require("babel")
 
 module.exports = {
   process: function(src, path) {
-    // CoffeeScript files can be .coffee, .litcoffee, or .coffee.md
-    if (coffee.helpers.isCoffee(path)) {
+    if (path.indexOf('node_modules/') >= 0) {
+      return src
+    } else if (coffee.helpers.isCoffee(path)) {
       return coffee.compile(src, {'bare': true})
     } else {
       return babel.transform(src).code
     }
-    return src
   }
 };
