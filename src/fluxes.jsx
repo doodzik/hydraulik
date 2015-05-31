4258005ea2dc9ab42a82173105a8b4fbfbc7e1c8
doodzik <frederik.dudzik@gmail.com>
2015-05-31 13:48:40 +0200

    rename files because they were named wrong

--- src/fluxes.jsx
+++ src/fluxes.jsx
@@ -1,9 +1,23 @@
-
 export default class Fluxes {
   constructor(storesBuild) {
+    this.fluxes   = {}
     this.setsBase = storesBuild.baseSets
     this.setsSub  = storesBuild.subSets
+
+
     // this.fluxes     = foreach storesBase init flux and saves it into {}
+    for (store in this.setsBase) {
+      if (!this.stores.hasOwnProperty(store)) continue
+      _store = this.setsBase[store]
+      this.fluxes[store] = new Flux(_store, this.dispatcher)
+    }
+
     // this.fluxes     = foreach storesSub  extend from flux parent flux and saves it into {}
+    for (store in this.setsSub) {
+      if (!this.stores.hasOwnProperty(store)) continue
+      _store    = this.setsSub[store]
+      baseEvent = this.fluxes[_store.subsetOf.name].events
+      this.fluxes[store] = new Flux(_store, this.dispatcher, baseEvent)
+    }
   }
 }
