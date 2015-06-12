ed5c2b5cfb910b5c9b393e512df0b1b8d8e51440
doodzik <frederik.dudzik@gmail.com>
2015-06-12 18:14:39 +0200

    remove observerKlass from export

--- src/hydraulik.jsx
+++ src/hydraulik.jsx
@@ -1,4 +1,3 @@
 export var Schema        = require('./schema')
 export var Set           = require('./set')
 export var Klass         = require('./klass')
-export var ObserverKlass = require('./observerKlass')
