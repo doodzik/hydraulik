f78f4287bdd6704652c2d5d6135b96023b90d40e
doodzik <frederik.dudzik@gmail.com>
2015-06-15 19:08:29 +0200

    write new schema implementation which uses es7 decorators and es6 class syntax

---
+++ src/__tests__/schema-new-test.js
@@ -0,0 +1,68 @@
+jest.dontMock('../schema-new')
+
+var Schema = require('../schema-new').default,
+    type   = require('../schema-new').type,
+    Str    = require('hydraulik-types').Str
+
+
+describe('Schema', function() {
+  it('#new sets the name', function() {
+    class Users extends Schema {}
+    var schema = new Users()
+    expect(schema.name).toEqual('users')
+  })
+
+  it('@type', function() {
+    @type(Str)
+    class Users extends Schema {}
+    var schema = new Users()
+    expect(schema.types).toEqual([
+      {
+        type: Str,
+        name: 'str'
+      }
+    ])
+  })
+
+  it('@type changes name of type', function() {
+    @type(Str, name = 'text')
+    class Users extends Schema {}
+    var schema = new Users()
+    expect(schema.types).toEqual([
+      {
+        type: Str,
+        name: 'text'
+      }
+    ])
+  })
+
+  it('@type is chainable', function() {
+    @type(Str)
+    @type(Str, name = 'text')
+    class Users extends Schema {}
+    var schema = new Users()
+    expect(schema.types).toEqual([
+      {
+        type: Str,
+        name: 'text'
+      }, {
+        type: Str,
+        name: 'str'
+      }
+    ])
+  })
+
+  it("#filter if filter isn't called than function that returns true", function() {
+    var schema = new Schema()
+    expect(schema.filter()).toEqual(true)
+  })
+
+  it("subsetOf", function() {
+    class Users  extends Schema {}
+    class Admins extends Users {}
+    var users  = new Users(),
+        admins = new Admins()
+    expect(users.baseSet).toEqual(false)
+    expect(admins.baseSet).toEqual('users')
+  })
+})
---
+++ src/schema-new.jsx
@@ -0,0 +1,39 @@
+import Set from './set'
+
+export var type = function (type, name = false) {
+   return function decorator(target) {
+      target.types = target.types || []
+      name         = name || type.name.toLowerCase()
+      target.types.push({ type, name })
+      return target
+   }
+}
+
+export default class Schema {
+  constructor() {
+    this.name = this.constructor.name.toLowerCase()
+    this.set  = Set
+    this.setBaseSet()
+  }
+
+  filter(val) {
+    return true
+  }
+
+  get types (){
+    return this.constructor.types
+  }
+
+  setBaseSet() {
+    var _this  = this.constructor,
+        _super = Object.getPrototypeOf(_this)
+
+    while(_super != Schema && _this != Schema) {
+      _this  = _super
+      _super = Object.getPrototypeOf(_this)
+    }
+
+    this.baseSet = (_this == this.constructor) ? false : new _this().name
+  }
+}
+
